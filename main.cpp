5bd6b98d3e2062204b3eaa698da9cdbd1d85add0
svihra <psvihra@gmail.com>
2018-08-19 21:44:24 +0200

    added options to main

--- main.cpp
+++ main.cpp
@@ -18,8 +18,10 @@ int main(int argc, char **argv)
     TString file = "";
     TString tree = "rawtree";
     UInt_t maxEntries = 0;
+    Int_t startPart = -1;
+    Int_t parts = 25;
 
-    while( ( c = getopt (argc, argv, ":bf:t:e:") ) != -1 )
+    while( ( c = getopt (argc, argv, ":bf:t:e:s:p:") ) != -1 )
     {
         switch(c)
         {
@@ -38,6 +40,12 @@ int main(int argc, char **argv)
             case 'e':
                 if(optarg){ maxEntries = (UInt_t) std::atoll(optarg); break;}
                 else {std::cout << "Define max entries " << std::endl;}
+            case 's':
+                if(optarg){ startPart = (UInt_t) std::atoi(optarg); break;}
+                else {std::cout << "Define start part " << std::endl;}
+            case 'p':
+                if(optarg){ parts = (UInt_t) std::atoi(optarg); break;}
+                else {std::cout << "Define number of parts " << std::endl;}
             case '?':
                 std::cout << "Received unknown argument " << c << std::endl;
             default :
@@ -47,12 +55,14 @@ int main(int argc, char **argv)
                 std::cout << "-f [] name of file to process " << std::endl;
                 std::cout << "-t [] rawtree/proctree" << std::endl;
                 std::cout << "-e [] max number of entries" << std::endl;
+                std::cout << "-s [] starting part" << std::endl;
+                std::cout << "-p [] number of parts" << std::endl;
                 return -1;
             break;
         }
     }
     if (bash)
-        new Entangled(file, tree, maxEntries);
+        new Entangled(file, tree, maxEntries, startPart, parts);
     else
     {
         TApplication theApp("App",&argc,argv);
