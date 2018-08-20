23f2f03b610c48f59013db917fd644d9e1fb8392
svihra <psvihra@gmail.com>
2018-08-20 14:49:31 +0200

    added for combining split csvs

---
+++ combine.C
@@ -0,0 +1,55 @@
+#include <TString.h>
+#include <TFile.h>
+#include <TH1.h>
+#include <TDirectory.h>
+
+#include <fstream>
+
+void combine(TString fileName)
+{
+    if (fileName.EndsWith("_combined_processed.root"))
+    {
+        std::cout << "Reading tree" << std::endl;
+        TFile* fileRoot = new TFile(fileName, "READ");
+
+        for (Int_t x1 = 0; x1 < 3; x1++)
+        {
+            for (Int_t y1 = 0; y1 < 3; y1++)
+            {
+                for (Int_t x2 = 0; x2 < 3; x2++)
+                {
+                    for (Int_t y2 = 0; y2 < 3; y2++)
+                    {
+                        TString csvName = fileName;
+                        csvName.Remove(csvName.Last('.'),200);
+                        csvName.ReplaceAll("combined_","");
+                        csvName.Append(Form("_%dx%d_%dx%d.csv",x1,y1,x2,y2));
+
+                        TString entName, single1Name, single2Name;
+                        entName.Form("histEnt_%dx%d_%dx%d",x1,y1,x2,y2);
+                        single1Name.Form("histSingle1_%dx%d_%dx%d",x1,y1,x2,y2);
+                        single2Name.Form("histSingle2_%dx%d_%dx%d",x1,y1,x2,y2);
+                        TH1F* ent = (TH1F*) fileRoot->Get(entName);
+                        TH1F* single1 = (TH1F*) fileRoot->Get(single1Name);
+                        TH1F* single2 = (TH1F*) fileRoot->Get(single2Name);
+
+                        std::ofstream csvFile;
+                        csvFile.open(csvName);
+
+                        for (Int_t bin = 1; bin <= ent->GetNbinsX(); bin++)
+                        {
+                            if (bin < ent->GetNbinsX()/2)
+                                csvFile << ent->GetBinCenter(bin) << "," << ent->GetBinContent(bin) << "," << single2->GetBinContent(bin) << "\n";
+                            else if (bin > 1 + ent->GetNbinsX()/2)
+                                csvFile << ent->GetBinCenter(bin) << "," << ent->GetBinContent(bin) << "," << single1->GetBinContent(bin) << "\n";
+                            else
+                                csvFile << ent->GetBinCenter(bin) << "," << ent->GetBinContent(bin) << "," << (single1->GetBinContent(bin)+single2->GetBinContent(bin)) << "\n";
+                        }
+                        csvFile.close();
+                    }
+                }
+            }
+        }
+    }
+    std::cout << "Finished " << fileName << std::endl;
+}
